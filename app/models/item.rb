0c5b352bbb8d8fbd6c0b530138359a4db153360d
Yasuhisa Matsushita <yw66zb@gmail.com>
2017-01-21 13:40:08 +0000

    first merge

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  #want_usersとの仮想的な中間テーブルとなる	外部キーとしてitem_idを用いる
+  has_many :wants, foreign_key: "item_id", dependent: :destroy
+  has_many :users, through: :wants
+  
+  #Itemをwantしたユーザーの一覧。wantsを用いて取得する	source: :userを用いる
+  has_many :wants, class_name: "Want", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users , through: :wants, source: :user
+  
+  #have_usersとの仮想的な中間テーブルとなる	外部キーとしてitem_idを用いる
+  has_many :haves, foreign_key: "item_id", dependent: :destroy
+  has_many :users, through: :haves
+  
+  #Itemをhaveしたユーザーの一覧。havesを用いて取得する	source: :userを用いる
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users , through: :haves, source: :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,14 @@ class User < ActiveRecord::Base
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_users, through: :followed_relationships, source: :follower
 
-  has_many :ownerships , foreign_key: "user_id", dependent: :destroy
-  has_many :items ,through: :ownerships
+  has_many :ownerships, foreign_key: "user_id", dependent: :destroy
+  has_many :items, through: :ownerships
 
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +36,28 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id) #指定している中間テーブルから見つける
   end
 
   def unhave(item)
+    item = haves.find_by(item_id: item.id)
+    item.destroy if item != nil
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    item = wants.find_by(item_id: item.id)
+    item.destroy if item != nil
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
